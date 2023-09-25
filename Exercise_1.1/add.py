e837fd003ea84acfb43cf2d54e06bc8a141b96ef
Priya <118628757+priya-km@users.noreply.github.com>
2023-09-25 18:39:48 -0400

    Create add.py

---
+++ Exercise_1.1/add.py
@@ -0,0 +1,11 @@
+# User enters first number
+a = int(input("First Number = "))
+
+# User enters second number
+b = int(input("Second number = "))
+
+# Both inputs get added together
+c = a + b
+
+# Print sum of two numbers
+print("Sum = ", c)
